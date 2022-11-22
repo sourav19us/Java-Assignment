efe93fd0657b82267d3e8a4d53c27f7d3255befc
sourav19us <sourav19us@gmail.com>
2022-11-22 14:21:04 +0530

    Add Comments and Improve NumInWorde file code

--- NumInWorde.java
+++ NumInWorde.java
@@ -9,8 +9,9 @@ public class NumInWorde {
         b = (Num / 100) - (a * 10);
         c = Num / 10 - ((a * 100) + (b * 10));
         d = Num - ((a * 1000) + (b * 100) + (c * 10));
+        System.out.println("Digits are from Right to Left");
+        System.out.println("1st digit = " + a + " ,2nd digit = " + b + " ,3rd digit = " + c + " ,4th digit = " + d);
         System.out.println("Num is = " + Num);
-        System.out.println("a = " + a + " ,b = " + b + " ,c = " + c + " ,d = " + d);
         switch (a) {
             case 0:
                 break;
@@ -79,6 +80,7 @@ public class NumInWorde {
             default:
                 break;
         }
+        // if 4th digit d is Zero and 3rd digit is not Zero
         if (d == 0) {
 
             if (c != 0) {
@@ -120,6 +122,7 @@ public class NumInWorde {
                 }
             }
         }
+        // if 3rd digit is 1 and 4th digit is not Zero
         if (c == 1) {
             if (d != 0) {
 
@@ -160,6 +163,7 @@ public class NumInWorde {
             }
 
         }
+        // if 3rd digit is zero but 4th digit is not Zero
         if (c == 0) {
             if (d != 0) {
 
@@ -199,301 +203,386 @@ public class NumInWorde {
                 }
             }
         }
-        if (c == 2) {
-            System.out.print("Twenty ");
-            switch (d) {
-                case 0:
-                    break;
-                case 1:
-                    System.out.print("one ");
-                    break;
-                case 2:
-                    System.out.print("Two ");
-                    break;
-                case 3:
-                    System.out.print("Three ");
-                    break;
-                case 4:
-                    System.out.print("Four ");
-                    break;
-                case 5:
-                    System.out.print("Five");
-                    break;
-                case 6:
-                    System.out.print("Six ");
-                    break;
-                case 7:
-                    System.out.print("Seven ");
-                    break;
-                case 8:
-                    System.out.print("Eight ");
-                    break;
-                case 9:
-                    System.out.print("Nine ");
-                    break;
+        // if 4th and 3rd digit are not Zero
+        if (d != 0) {
 
-                default:
-                    break;
-            }
-        }
-        if (c == 3) {
-            System.out.print("Thirty ");
-            switch (d) {
-                case 0:
-                    break;
-                case 1:
-                    System.out.print("one ");
-                    break;
-                case 2:
-                    System.out.print("Two ");
-                    break;
-                case 3:
-                    System.out.print("Three ");
-                    break;
-                case 4:
-                    System.out.print("Four ");
-                    break;
-                case 5:
-                    System.out.print("Five");
-                    break;
-                case 6:
-                    System.out.print("Six ");
-                    break;
-                case 7:
-                    System.out.print("Seven ");
-                    break;
-                case 8:
-                    System.out.print("Eight ");
-                    break;
-                case 9:
-                    System.out.print("Nine ");
-                    break;
+            if (c != 0) {
+                // if 3rd digit is 2 and 4th digit is not zero
+                if (c == 2) {
+                    System.out.print("Twenty ");
+                }
+                // if 3rd digit is 3 and 4th digit is not zero
+                if (c == 3) {
+                    System.out.print("Thirty ");
+                }
+                // if 3rd digit is 4 and 4th digit is not zero
+                if (c == 4) {
+                    System.out.print("Fourty ");
+                }
+                // if 3rd digit is 5 and 4th digit is not zero
+                if (c == 5) {
+                    System.out.print("Fifty ");
+                }
+                // if 3rd digit is 6 and 4th digit is not zero
+                if (c == 6) {
+                    System.out.print("Sixty ");
+                }
+                // if 3rd digit is 7 and 4th digit is not zero
+                if (c == 7) {
+                    System.out.print("Seventy ");
+                }
+                // if 3rd digit is 8 and 4th digit is not zero
+                if (c == 8) {
+                    System.out.print("Eighty ");
+                }
+                // if 3rd digit is 9 and 4th digit is not zero
+                if (c == 9) {
+                    System.out.print("Ninty ");
+                }
+                switch (d) {
+                    case 0:
+                        break;
+                    case 1:
+                        System.out.print("one ");
+                        break;
+                    case 2:
+                        System.out.print("Two ");
+                        break;
+                    case 3:
+                        System.out.print("Three ");
+                        break;
+                    case 4:
+                        System.out.print("Four ");
+                        break;
+                    case 5:
+                        System.out.print("Five");
+                        break;
+                    case 6:
+                        System.out.print("Six ");
+                        break;
+                    case 7:
+                        System.out.print("Seven ");
+                        break;
+                    case 8:
+                        System.out.print("Eight ");
+                        break;
+                    case 9:
+                        System.out.print("Nine ");
+                        break;
 
-                default:
-                    break;
+                    default:
+                        break;
+                }
             }
-        }
-        if (c == 4) {
-            System.out.print("Tourty ");
-            switch (d) {
-                case 0:
-                    break;
-                case 1:
-                    System.out.print("one ");
-                    break;
-                case 2:
-                    System.out.print("Two ");
-                    break;
-                case 3:
-                    System.out.print("Three ");
-                    break;
-                case 4:
-                    System.out.print("Four ");
-                    break;
-                case 5:
-                    System.out.print("Five");
-                    break;
-                case 6:
-                    System.out.print("Six ");
-                    break;
-                case 7:
-                    System.out.print("Seven ");
-                    break;
-                case 8:
-                    System.out.print("Eight ");
-                    break;
-                case 9:
-                    System.out.print("Nine ");
-                    break;
 
-                default:
-                    break;
-            }
         }
-        if (c == 5) {
-            System.out.print("Fifty ");
-            switch (d) {
-                case 0:
-                    break;
-                case 1:
-                    System.out.print("one ");
-                    break;
-                case 2:
-                    System.out.print("Two ");
-                    break;
-                case 3:
-                    System.out.print("Three ");
-                    break;
-                case 4:
-                    System.out.print("Four ");
-                    break;
-                case 5:
-                    System.out.print("Five");
-                    break;
-                case 6:
-                    System.out.print("Six ");
-                    break;
-                case 7:
-                    System.out.print("Seven ");
-                    break;
-                case 8:
-                    System.out.print("Eight ");
-                    break;
-                case 9:
-                    System.out.print("Nine ");
-                    break;
 
-                default:
-                    break;
-            }
-        }
-        if (c == 6) {
-            System.out.print("Sixty ");
-            switch (d) {
-                case 0:
-                    break;
-                case 1:
-                    System.out.print("one ");
-                    break;
-                case 2:
-                    System.out.print("Two ");
-                    break;
-                case 3:
-                    System.out.print("Three ");
-                    break;
-                case 4:
-                    System.out.print("Four ");
-                    break;
-                case 5:
-                    System.out.print("Five");
-                    break;
-                case 6:
-                    System.out.print("Six ");
-                    break;
-                case 7:
-                    System.out.print("Seven ");
-                    break;
-                case 8:
-                    System.out.print("Eight ");
-                    break;
-                case 9:
-                    System.out.print("Nine ");
-                    break;
-
-                default:
-                    break;
-            }
-        }
-        if (c == 7) {
-            System.out.print("Seventy ");
-            switch (d) {
-                case 0:
-                    break;
-                case 1:
-                    System.out.print("one ");
-                    break;
-                case 2:
-                    System.out.print("Two ");
-                    break;
-                case 3:
-                    System.out.print("Three ");
-                    break;
-                case 4:
-                    System.out.print("Four ");
-                    break;
-                case 5:
-                    System.out.print("Five");
-                    break;
-                case 6:
-                    System.out.print("Six ");
-                    break;
-                case 7:
-                    System.out.print("Seven ");
-                    break;
-                case 8:
-                    System.out.print("Eight ");
-                    break;
-                case 9:
-                    System.out.print("Nine ");
-                    break;
-
-                default:
-                    break;
-            }
-        }
-        if (c == 8) {
-            System.out.print("Eighty ");
-            switch (d) {
-                case 0:
-                    break;
-                case 1:
-                    System.out.print("one ");
-                    break;
-                case 2:
-                    System.out.print("Two ");
-                    break;
-                case 3:
-                    System.out.print("Three ");
-                    break;
-                case 4:
-                    System.out.print("Four ");
-                    break;
-                case 5:
-                    System.out.print("Five");
-                    break;
-                case 6:
-                    System.out.print("Six ");
-                    break;
-                case 7:
-                    System.out.print("Seven ");
-                    break;
-                case 8:
-                    System.out.print("Eight ");
-                    break;
-                case 9:
-                    System.out.print("Nine ");
-                    break;
-
-                default:
-                    break;
-            }
-        }
-        if (c == 9) {
-            System.out.print("Ninty ");
-            switch (d) {
-                case 0:
-                    break;
-                case 1:
-                    System.out.print("one ");
-                    break;
-                case 2:
-                    System.out.print("Two ");
-                    break;
-                case 3:
-                    System.out.print("Three ");
-                    break;
-                case 4:
-                    System.out.print("Four ");
-                    break;
-                case 5:
-                    System.out.print("Five");
-                    break;
-                case 6:
-                    System.out.print("Six ");
-                    break;
-                case 7:
-                    System.out.print("Seven ");
-                    break;
-                case 8:
-                    System.out.print("Eight ");
-                    break;
-                case 9:
-                    System.out.print("Nine ");
-                    break;
-
-                default:
-                    break;
-            }
-        }
     }
 }
+/*
+ * Second Method for last tow Digit
+ * // if 3rd digit is 2 and 4th digit is not zero
+ * if (c == 2) {
+ * System.out.print("Twenty ");
+ * switch (d) {
+ * case 0:
+ * break;
+ * case 1:
+ * System.out.print("one ");
+ * break;
+ * case 2:
+ * System.out.print("Two ");
+ * break;
+ * case 3:
+ * System.out.print("Three ");
+ * break;
+ * case 4:
+ * System.out.print("Four ");
+ * break;
+ * case 5:
+ * System.out.print("Five");
+ * break;
+ * case 6:
+ * System.out.print("Six ");
+ * break;
+ * case 7:
+ * System.out.print("Seven ");
+ * break;
+ * case 8:
+ * System.out.print("Eight ");
+ * break;
+ * case 9:
+ * System.out.print("Nine ");
+ * break;
+ * 
+ * default:
+ * break;
+ * }
+ * }
+ * // if 3rd digit is 3 and 4th digit is not zero
+ * if (c == 3) {
+ * System.out.print("Thirty ");
+ * switch (d) {
+ * case 0:
+ * break;
+ * case 1:
+ * System.out.print("one ");
+ * break;
+ * case 2:
+ * System.out.print("Two ");
+ * break;
+ * case 3:
+ * System.out.print("Three ");
+ * break;
+ * case 4:
+ * System.out.print("Four ");
+ * break;
+ * case 5:
+ * System.out.print("Five");
+ * break;
+ * case 6:
+ * System.out.print("Six ");
+ * break;
+ * case 7:
+ * System.out.print("Seven ");
+ * break;
+ * case 8:
+ * System.out.print("Eight ");
+ * break;
+ * case 9:
+ * System.out.print("Nine ");
+ * break;
+ * 
+ * default:
+ * break;
+ * }
+ * }
+ * // if 3rd digit is 4 and 4th digit is not zero
+ * if (c == 4) {
+ * System.out.print("Tourty ");
+ * switch (d) {
+ * case 0:
+ * break;
+ * case 1:
+ * System.out.print("one ");
+ * break;
+ * case 2:
+ * System.out.print("Two ");
+ * break;
+ * case 3:
+ * System.out.print("Three ");
+ * break;
+ * case 4:
+ * System.out.print("Four ");
+ * break;
+ * case 5:
+ * System.out.print("Five");
+ * break;
+ * case 6:
+ * System.out.print("Six ");
+ * break;
+ * case 7:
+ * System.out.print("Seven ");
+ * break;
+ * case 8:
+ * System.out.print("Eight ");
+ * break;
+ * case 9:
+ * System.out.print("Nine ");
+ * break;
+ * 
+ * default:
+ * break;
+ * }
+ * }
+ * // if 3rd digit is 5 and 4th digit is not zero
+ * if (c == 5) {
+ * System.out.print("Fifty ");
+ * switch (d) {
+ * case 0:
+ * break;
+ * case 1:
+ * System.out.print("one ");
+ * break;
+ * case 2:
+ * System.out.print("Two ");
+ * break;
+ * case 3:
+ * System.out.print("Three ");
+ * break;
+ * case 4:
+ * System.out.print("Four ");
+ * break;
+ * case 5:
+ * System.out.print("Five");
+ * break;
+ * case 6:
+ * System.out.print("Six ");
+ * break;
+ * case 7:
+ * System.out.print("Seven ");
+ * break;
+ * case 8:
+ * System.out.print("Eight ");
+ * break;
+ * case 9:
+ * System.out.print("Nine ");
+ * break;
+ * 
+ * default:
+ * break;
+ * }
+ * }
+ * // if 3rd digit is 6 and 4th digit is not zero
+ * if (c == 6) {
+ * System.out.print("Sixty ");
+ * switch (d) {
+ * case 0:
+ * break;
+ * case 1:
+ * System.out.print("one ");
+ * break;
+ * case 2:
+ * System.out.print("Two ");
+ * break;
+ * case 3:
+ * System.out.print("Three ");
+ * break;
+ * case 4:
+ * System.out.print("Four ");
+ * break;
+ * case 5:
+ * System.out.print("Five");
+ * break;
+ * case 6:
+ * System.out.print("Six ");
+ * break;
+ * case 7:
+ * System.out.print("Seven ");
+ * break;
+ * case 8:
+ * System.out.print("Eight ");
+ * break;
+ * case 9:
+ * System.out.print("Nine ");
+ * break;
+ * 
+ * default:
+ * break;
+ * }
+ * }
+ * // if 3rd digit is 7 and 4th digit is not zero
+ * if (c == 7) {
+ * System.out.print("Seventy ");
+ * switch (d) {
+ * case 0:
+ * break;
+ * case 1:
+ * System.out.print("one ");
+ * break;
+ * case 2:
+ * System.out.print("Two ");
+ * break;
+ * case 3:
+ * System.out.print("Three ");
+ * break;
+ * case 4:
+ * System.out.print("Four ");
+ * break;
+ * case 5:
+ * System.out.print("Five");
+ * break;
+ * case 6:
+ * System.out.print("Six ");
+ * break;
+ * case 7:
+ * System.out.print("Seven ");
+ * break;
+ * case 8:
+ * System.out.print("Eight ");
+ * break;
+ * case 9:
+ * System.out.print("Nine ");
+ * break;
+ * 
+ * default:
+ * break;
+ * }
+ * }
+ * // if 3rd digit is 8 and 4th digit is not zero
+ * if (c == 8) {
+ * System.out.print("Eighty ");
+ * switch (d) {
+ * case 0:
+ * break;
+ * case 1:
+ * System.out.print("one ");
+ * break;
+ * case 2:
+ * System.out.print("Two ");
+ * break;
+ * case 3:
+ * System.out.print("Three ");
+ * break;
+ * case 4:
+ * System.out.print("Four ");
+ * break;
+ * case 5:
+ * System.out.print("Five");
+ * break;
+ * case 6:
+ * System.out.print("Six ");
+ * break;
+ * case 7:
+ * System.out.print("Seven ");
+ * break;
+ * case 8:
+ * System.out.print("Eight ");
+ * break;
+ * case 9:
+ * System.out.print("Nine ");
+ * break;
+ * 
+ * default:
+ * break;
+ * }
+ * }
+ * // // if 3rd digit is 9 and 4th digit is not zero
+ * if (c == 9) {
+ * System.out.print("Ninty ");
+ * switch (d) {
+ * case 0:
+ * break;
+ * case 1:
+ * System.out.print("one ");
+ * break;
+ * case 2:
+ * System.out.print("Two ");
+ * break;
+ * case 3:
+ * System.out.print("Three ");
+ * break;
+ * case 4:
+ * System.out.print("Four ");
+ * break;
+ * case 5:
+ * System.out.print("Five");
+ * break;
+ * case 6:
+ * System.out.print("Six ");
+ * break;
+ * case 7:
+ * System.out.print("Seven ");
+ * break;
+ * case 8:
+ * System.out.print("Eight ");
+ * break;
+ * case 9:
+ * System.out.print("Nine ");
+ * break;
+ * 
+ * default:
+ * break;
+ * }
+ * }
+ */
