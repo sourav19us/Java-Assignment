2f281d6884a802493eff6b3ab02a6e028b9ab444
sourav19us <sourav19us@gmail.com>
2022-12-07 13:26:08 +0530

    Add file

--- RationalNo.java
+++ RationalNo.java
@@ -14,4 +14,33 @@ public class RationalNo {
         System.out.println("b = " + b);
         System.out.println(a + "/" + b);
     }
+
+    public RationalNo add(RationalNo ref) {
+        RationalNo temp = new RationalNo();
+        temp.a = a * ref.b + b * ref.a;
+        temp.b = b * ref.b;
+        return temp;
+    }
+
+    public RationalNo less(RationalNo ref) {
+        RationalNo temp = new RationalNo();
+        temp.a = a * ref.b - b * ref.a;
+        temp.b = b * ref.b;
+        return temp;
+    }
+
+    public RationalNo mult(RationalNo ref) {
+        RationalNo temp = new RationalNo();
+        temp.a = a * ref.a;
+        temp.b = b * ref.b;
+        return temp;
+    }
+
+    public RationalNo divide(RationalNo ref) {
+        RationalNo temp = new RationalNo();
+        temp.a = a * ref.b;
+        temp.b = b * ref.a;
+        return temp;
+    }
+
 }
--- UseRational.java
+++ UseRational.java
@@ -3,7 +3,19 @@
 public class UseRational {
     public static void main(String[] args) {
         RationalNo obj = new RationalNo();
+        RationalNo obj2 = new RationalNo();
+        RationalNo obj3 = new RationalNo();
         obj.set(2, 5);
+        obj2.set(2, 5);
+        obj3 = obj.add(obj2);
         obj.get();
+        obj2.get();
+        obj3.get();
+        obj3 = obj.less(obj2);
+        obj3.get();
+        obj3 = obj.mult(obj2);
+        obj3.get();
+        obj3 = obj.divide(obj2);
+        obj3.get();
     }
 }
